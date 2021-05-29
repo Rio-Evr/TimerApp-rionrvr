df60e8f24e8bbdadf1924d82db21de87723dba25
Rio.fx <64973439+Rio-Evr@users.noreply.github.com>
2021-05-29 09:51:33 +0900

    Add TimerApp Folder
    Created TimerApp

---
+++ TimerApp/TimerApp/TimerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerAppApp.swift
+//  TimerApp
+//
+//  Created by FX on 2021/05/29.
+//
+
+import SwiftUI
+
+@main
+struct TimerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
